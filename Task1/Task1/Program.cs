6833d684e870fb68a38c684caeecb9875c952bcb
Yevgeniy Asseyev <yevgeniy_asseyev@epam.com>
2018-05-10 14:18:59 +0600

    First commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Task1
+{
+    using System;
+    using System.Diagnostics;
+    using System.Security.Cryptography;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            var itercount = 100;
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < itercount; i++)
+            {
+                GeneratePasswordHashUsingSalt("superPassword", salt);
+            }
+
+            sw.Stop();
+            Console.WriteLine("Not optimized Time:" + sw.Elapsed.TotalMilliseconds + "ms");
+            sw = Stopwatch.StartNew();
+            for (int i = 0; i < itercount; i++)
+            {
+                OptimizedGeneratePasswordHashUsingSalt("superPassword", salt);
+            }
+
+            sw.Stop();
+            Console.WriteLine("Optimized Time:" + sw.Elapsed.TotalMilliseconds + "ms");
+            Console.ReadLine();
+        }
+
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            var passwordHash = Convert.ToBase64String(hashBytes);
+            return passwordHash;
+        }
+
+        public static async Task<string> OptimizedGeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = await GetBytes(pbkdf2);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, hashBytes, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            var passwordHash = Convert.ToBase64String(hashBytes);
+            return passwordHash;
+        }
+
+        private static Task<byte[]> GetBytes(Rfc2898DeriveBytes rfc2898)
+        {
+            return Task.Run(() => rfc2898.GetBytes(20));
+        }
+    }
+}
---
+++ Task2/ProfileSample/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProfileSample.DAL;
+using ProfileSample.Models;
+
+namespace ProfileSample.Controllers
+{
+    using System.Data.Entity;
+    using System.Threading.Tasks;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var context = new ProfileSampleEntities();
+
+            var sources = context.ImgSources.Take(20).Select(x => x.Id);
+            
+            var model = new List<ImageModel>();
+
+            foreach (var id in sources)
+            {
+                var item = context.ImgSources.Find(id);
+
+                var obj = new ImageModel()
+                {
+                    Name = item.Name,
+                    Data = item.Data
+                };
+                
+                model.Add(obj);
+            } 
+
+            return View(model);
+        }
+
+        public async Task<ActionResult> IndexOptimized()
+        {
+            var model = new List<NewImageModel>();
+            using (var context = new ProfileSampleEntities())
+            {
+                var source = await context.ImgSources.Take(20).ToArrayAsync();
+                model = source.Select(x => new NewImageModel()
+                                               {
+                                                   Name = x.Name,
+                                                   Data = string.Format("data:image/jpg;base64,{0}", System.Convert.ToBase64String(x.Data))
+                                               }).ToList();
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Convert()
+        {
+            var files = Directory.GetFiles(Server.MapPath("~/Content/Img"), "*.jpg");
+
+            using (var context = new ProfileSampleEntities())
+            {
+                foreach (var file in files)
+                {
+                    using (var stream = new FileStream(file, FileMode.Open))
+                    {
+                        byte[] buff = new byte[stream.Length];
+
+                        stream.Read(buff, 0, (int) stream.Length);
+
+                        var entity = new ImgSource()
+                        {
+                            Name = Path.GetFileName(file),
+                            Data = buff,
+                        };
+
+                        context.ImgSources.Add(entity);
+                        context.SaveChanges();
+                    }
+                } 
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
